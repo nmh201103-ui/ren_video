4e517b476c41f8f855ef1ee7c6999899b1f135ba
Nguyenhoan03 <phephehoan@gmail.com>
2026-02-01 23:16:17 +0700

    update

--- scripts/check_renderer_llm.py
+++
@@ -1,6 +0,0 @@
-import os
-os.environ['LLM_PROVIDER'] = 'ollama'
-from video.render import SmartVideoRenderer
-r = SmartVideoRenderer()
-print('Script generator class:', r.script_generator.__class__.__name__)
-print('SceneSelector llm_client set:', r.scene_selector.llm_client is not None)
